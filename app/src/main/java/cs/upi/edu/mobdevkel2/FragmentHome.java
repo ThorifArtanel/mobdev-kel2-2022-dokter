37502b4e4cc19218d736f2d96c48acf3d88cc2e7
Thorif Artanel <thorif.m.artanel@gmail.com>
2022-10-23 22:07:28 +0700

    Nav Bar Update

---
+++ app/src/main/java/cs/upi/edu/mobdevkel2/FragmentHome.java
@@ -0,0 +1,31 @@
+package cs.upi.edu.mobdevkel2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class FragmentHome extends Fragment{
+    public FragmentHome(){
+        // require a empty public constructor
+    }
+
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_home, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
--- app/src/main/java/cs/upi/edu/mobdevkel2/MainActivity.java
+++ app/src/main/java/cs/upi/edu/mobdevkel2/MainActivity.java
@@ -3,12 +3,42 @@ package cs.upi.edu.mobdevkel2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import cs.upi.edu.mobdevkel2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bnv;
+    FragmentHome fragmentHome = new FragmentHome();
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
         setContentView(R.layout.activity_main);
+
+        bnv = binding.botNav;
+        bnv.setSelectedItemId(R.id.home);
+        bnv.setOnItemSelectedListener(
+                item -> {
+                    int id = item.getItemId();
+                    switch (id) {
+                        case R.id.home:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentHome).commit();
+                            return true;
+                        case R.id.lansia:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentHome).commit();
+                            return true;
+                    }
+
+                    return true;
+                });
     }
+
 }
