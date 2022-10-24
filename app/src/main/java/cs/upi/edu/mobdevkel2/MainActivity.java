50e2695100cece762a55eb1cc64c4e711ec40e0f
Thorif Artanel <thorif.m.artanel@gmail.com>
2022-10-24 20:41:32 +0700

    Fix Nav Bar & Main Activity Fragment

--- app/src/main/java/cs/upi/edu/mobdevkel2/MainActivity.java
+++ app/src/main/java/cs/upi/edu/mobdevkel2/MainActivity.java
@@ -1,44 +1,46 @@
 package cs.upi.edu.mobdevkel2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import cs.upi.edu.mobdevkel2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bnv;
     FragmentHome fragmentHome = new FragmentHome();
-    private ActivityMainBinding binding;
+    FragmentLansia fragmentLansia = new FragmentLansia();
+    FragmentGuide fragmentGuide = new FragmentGuide();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
-
         setContentView(R.layout.activity_main);
 
-        bnv = binding.botNav;
+        bnv = findViewById(R.id.botNav);
         bnv.setSelectedItemId(R.id.home);
         bnv.setOnItemSelectedListener(
-                item -> {
-                    int id = item.getItemId();
-                    switch (id) {
-                        case R.id.home:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentHome).commit();
-                            return true;
-                        case R.id.lansia:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentHome).commit();
-                            return true;
-                    }
+                new BottomNavigationView.OnItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.home:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentHome).commit();
+                                return true;
+                            case R.id.lansia:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentLansia).commit();
+                                return true;
+                            case R.id.guide:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentGuide).commit();
+                                return true;
+                        }
 
-                    return true;
+                        return true;
+                    }
                 });
     }
-
 }
